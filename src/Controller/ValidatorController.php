d5e59c94a82f8f101437ac07ace3695210d0604d
malik.aouane <malik.aouane@le-campus-numerique.fr>
2020-09-21 11:53:46 +0200

    first commit project

---
+++ src/Controller/ValidatorController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ValidatorController extends AbstractController
+{
+/**
+ * @Route ("/index", name="page d'acceil")
+ */
+}
